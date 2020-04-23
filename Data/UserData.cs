9953ab4e71d7d35691bfedc193940f6eedb2c6f0
EZroot <utohaha@gmail.com>
2020-04-23 21:21:02 -0230

    added: tracking active people
    checks author of messages, gets their info and creates user data based on that

--- Data/UserData.cs
+++ Data/UserData.cs
@@ -6,14 +6,23 @@ namespace AshBot.Data
 {
     public class UserData
     {
+        ulong id;
         string username;
         string role;
         int level;
         int xp;
         int xpTillLevel;
 
-        public UserData(string username, string role, int level, int xp, int xpTillLevel)
+        public string Username { get => username; set => username = value; }
+        public string Role { get => role; set => role = value; }
+        public int Level { get => level; set => level = value; }
+        public int Xp { get => xp; set => xp = value; }
+        public int XpTillLevel { get => xpTillLevel; set => xpTillLevel = value; }
+        public ulong Id { get => id; set => id = value; }
+
+        public UserData(ulong id, string username, string role, int level, int xp, int xpTillLevel)
         {
+            this.id = id;
             this.username = username;
             this.role = role;
             this.level = level;
--- Services/JudgementService.cs
+++ Services/JudgementService.cs
@@ -1,21 +1,29 @@
-﻿using Discord;
+﻿using AshBot.Data;
+using Discord;
 using Discord.Commands;
 using Discord.WebSocket;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace AshBot.Services
 {
     public class JudgementService
     {
+        const string ROLE_DICTATOR = "DICTATOR";
+
         private readonly DiscordSocketClient _discord;
         private readonly CommandService _commands;
 
         private string _userDirectory { get; }
-        private string _userFile => Path.Combine(_userDirectory, $"userData.txt");
+        private string _userFile => Path.Combine(_userDirectory, $"UserData.json");
+
+        //Get our users
+        List<UserData> _userDataList = new List<UserData>();
 
         // DiscordSocketClient and CommandService are injected automatically from the IServiceProvider
         public JudgementService(DiscordSocketClient discord, CommandService commands)
@@ -35,15 +43,34 @@ namespace AshBot.Services
             if (!File.Exists(_userFile))               // Create today's log file if it doesn't exist
                 File.Create(_userFile).Dispose();
 
-            //Check the user who sent the message,
-            //Find out if we already have him stored as a "player"
+            SocketGuildUser su = msg.Author as SocketGuildUser;                          //User who sent the message
+            SocketUserMessage suMsg = msg as SocketUserMessage;                         //Socket user message
+            SocketCommandContext context = new SocketCommandContext(_discord, suMsg);  //Context of the message
+
+            //Create temp user data
+            UserData userData = new UserData(su.Id, msg.Author.Username, String.Join(",", su.Roles.Select(p => p.ToString()).ToArray()), 99, 1337, 2500);
+
+            //Add user to our active user list
+            bool containsItem = _userDataList.Any(item => item.Id == userData.Id); //compare userID to every userID in list to see if we exist
+            string result = "";
+            if (!containsItem)
+            {
+                _userDataList.Add(userData);
+                result = $"{DateTime.UtcNow.ToString("hh:mm:ss")} [Judgement] " +userData.Username + "-> Added to active user list";
+            }
+            else
+            {
+                result = $"{DateTime.UtcNow.ToString("hh:mm:ss")} [Judgement] User already exists in our active list!";
+            }
+
+            //Serialize our user
+            string jsonString = JsonConvert.SerializeObject(userData, Formatting.Indented);
+            //Find out if we already have him stored as a "player" before we append
+            File.WriteAllText(_userFile, jsonString);     // Write the user text to a file
             //if not, create him as a default player
             //check his message
             //update his XP/Level
-            string userText = $"User "+msg.Author.Username+" Sent a message. We need to judge him.";
-            File.AppendAllText(_userFile, userText + "\n");     // Write the user text to a file
-
-            return Console.Out.WriteLineAsync(userText);       // Write the log text to the console
+            return Console.Out.WriteLineAsync(result);       // Write the log text to the console
         }
     }
 }
