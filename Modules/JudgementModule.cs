aa1b1e15b8ca1128a31c4cf4806274aa56a46361
EZroot <utohaha@gmail.com>
2020-04-24 00:44:49 -0230

    added: judge command to remove XP from people

--- Modules/JudgementModule.cs
+++ Modules/JudgementModule.cs
@@ -12,12 +12,27 @@ namespace AshBot.Modules
 {
     public class JudgementModule : ModuleBase<SocketCommandContext>
     {
+        Random rand = new Random();
+
         [Command("stats")]
         [Summary("Check your stats.")]
         public async Task CheckStats()
         {
             UserData userData = JudgementService._activeUserList.Single(item => item.Id == Context.User.Id);
             await ReplyAsync(":exclamation:" + userData.Username + ":exclamation:\n.Level: " + userData.Level + "\n.XP:    " + userData.Xp+"\n.Goal:"+userData.XpTillLevel);
+        }
+
+        [Command("judge")]
+        [Summary("Judge em.")]
+        public async Task JudgeUser(SocketUser user = null)
+        {
+            string result = "";
+            if (user == null)
+                user = Context.User;
+
+            UserData userData = JudgementService._activeUserList.Single(item => item.Id == user.Id);
+            result = "You shall be JUDGED " + userData.Username + "! By the power of Runescape, I smite you for " + (userData.Level * 9 + rand.Next(userData.Level + 4)) + "\n dmg. Your xp is now " + userData.Xp + ".";
+            await ReplyAsync(result);
         }
     }
 }
