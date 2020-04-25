7165222314b0995d0e7718e280c369b5f300f7c5
EZroot <utohaha@gmail.com>
2020-04-25 13:58:59 -0230

    added: !help shows commands/music commands/motd/etc

--- Modules/InfoModule.cs
+++ Modules/InfoModule.cs
@@ -19,13 +19,43 @@ namespace AshBot.Modules
         [Summary("Help message.")]
         public async Task SayHelp()
         {
-            string s = "__***Information***__" +
-          "\n```diff\n+ [MOTD] Praise the sun! Welcome to church. Here, you gain levels by being a good, nice boy.\n```" +
-          "\n```diff\n- [Coming Soon] Role based commands to use against other players and ruin their XP gains.\n```" +
-          "\n***__Commands__***" +
-          "\n```yaml\n!stats - shows your current level and xp\n!slap @user - slap a noob <100xp>\n!heal @user - give healing <100xp>\n!judge @user - to judge someone <200xp>\n!sacrifice @user - sacrifice souls for the greater good <200xp>\n```" +
-          "\n***__Music Info__***" +
-          "\n```fix\n!play songname.mp3 - plays a song\n!stop - stops playing music\n!minsound - min volume\n!maxsound - max volume\n!normalsound - normal volume\n```" +
+            string s = "> ***__MOTD__***\n> Welcome to __***Church Of Sun***__.\n> Here, you gain levels by being a good, nice boy." +
+          "\n\n```Latest Update Info```" +
+          "\n```diff\n+ [Added] Audio streaming, Role-based commands for Angel/ArchAngel(s)\n```" +
+          "```diff\n- [Coming Soon] Role based commands to use against other players and ruin their XP gains.\n```" +
+          "\n***__More Help__***" +
+          "\n```yaml\n!motd - message of the day\n!stats - shows your current level and xp\n!commands - shows commands you can use against other people\n!music - shows a list of available music commands\n!songs - shows a list of available songs to stream\n```";
+            await ReplyAsync(s);
+        }
+
+        [Command("motd")]
+        public async Task MOTDCommand()
+        {
+            string s = "> ***__MOTD__***\n> Welcome to __***Church Of Sun***__.\n> Here, you gain levels by being a good, nice boy.";
+            await ReplyAsync(s);
+        }
+
+        [Command("commands")]
+        public async Task Commands()
+        {
+            string s = "***__Commands__***" +
+            "\n```yaml\n!stats - shows your current level and xp\n!slap @user - slap a noob <100xp>\n!heal @user - give healing <100xp>\n!judge @user - to judge someone <200xp>\n!sacrifice @user - sacrifice souls for the greater good <200xp>\n```";
+            await ReplyAsync(s);
+        }
+
+        [Command("musiccommands")]
+        public async Task MusicCommands()
+        {
+            string s = "***__Music Info__***" +
+          "\n```fix\n!play songname.mp3 - plays a song\n!stop - stops playing music\n!minsound - min volume\n!maxsound - max volume\n!normalsound - normal volume\n```";
+            await ReplyAsync(s);
+        }
+
+        [Command("music")]
+        public async Task Musics()
+        {
+            string s = "***__Music Info__***" +
+          "\n```fix\n!play songname.mp3 - plays a song\n!stop - stops playing music\n!minsound - min volume\n!maxsound - max volume\n!normalsound - normal volume\n```"+
           "\n***__Songs__***";
 
             string[] list = Directory.GetFiles(Path.Combine(AppContext.BaseDirectory, "usermusic"));
@@ -33,7 +63,19 @@ namespace AshBot.Modules
             s += "\n```css\n";
             s += String.Join("\n- ", list.Select(file => Path.GetFileName(file)).ToArray());
             s += "```";
-            await Console.Out.WriteLineAsync(s);
+            await ReplyAsync(s);
+        }
+
+        [Command("songs")]
+        public async Task SongList()
+        {
+            string s = "***__Songs__***";
+
+            string[] list = Directory.GetFiles(Path.Combine(AppContext.BaseDirectory, "usermusic"));
+
+            s += "\n```css\n";
+            s += String.Join("\n- ", list.Select(file => Path.GetFileName(file)).ToArray());
+            s += "```";
             await ReplyAsync(s);
         }
     }
